b24ed878e707e101e0baf10a26ee340c936b625d
MegijaKristaMilune <125242855+MegijaKristaMilune@users.noreply.github.com>
2023-03-18 09:34:03 +0000

    try

--- build_heap.py
+++ build_heap.py
@@ -4,46 +4,51 @@ def build_heap(data):
     swaps = []
     n = len(data)
   
+
     for i in range(n // 2 -1, -1, -1):
         heap(data, i, n, swaps)
     return swaps
 
- def heap(data, i, n, swaps):
-    pos_left = 2*i+1
-    pos_right = 2*i+2
-    last = i
 
-    if pos_left < n and data[pos_left] < data[i]:
-        last = pos_left
 
-    if pos_right < n and data[pos_right] < data[last]:
-        last = pos_right
 
-    if last != i:
+                
 
-        data[i], data[last] = data[last], data[i]
+def heap(data, i, n, swaps):
+    left_child = 2*i+1
+    right_child = 2*i+2
+    smallest = i
 
-        swaps.append((i, last))
+    if left_child < n and data[left_child] < data[i]:
+        smallest = left_child
 
-        heap(data, last, n, swaps)
+    if right_child < n and data[right_child] < data[smallest]:
+        smallest = right_child
+
+    if smallest != i:
+
+        data[i], data[smallest] = data[smallest], data[i]
+
+        swaps.append((i, smallest))
+
+        heap(data, smallest, n, swaps)
 
 
 def main():
 
-    input_method = (input("enter input method I/F: "))
+    input_method = (input("Enter input method (I for keyboard input, F for file input): "))
 
     if "I" in input_method:
         n = int(input("enter number of elements: "))
         data = list(map(int, input("enter the elements: ").split()))
-
     if "F" in input_method:
         filename = input("enter the filename:")
-        
         with open(f"tests/{filename}") as file:
             n = int(file.readline())
             data = list(map(int, file.readline().split()))
 
 
+    
     assert len(data) == n
 
    
@@ -51,7 +56,10 @@ def main():
     
     print(len(swaps))
     for i, j  in swaps:
+
+
         print(i, j)
+
 
 
 if __name__ == "__main__":
