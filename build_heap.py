4a4b497f59edbc64b4ca15a0f291ae436d0a6b42
MegijaKristaMilune <125242855+MegijaKristaMilune@users.noreply.github.com>
2023-03-18 09:09:46 +0000

    test3

--- build_heap.py
+++ build_heap.py
@@ -1,54 +1,92 @@
+# python3
+
 def build_heap(data):
     swaps = []
+    n = len(data)
+  
 
-    for i in range(len(data) // 2 - 1, -1, -1):
-        heapify(data, i, len(data), swaps)
+    # for i in range(n // 2 -1, -1, -1):
+    #     heapify(data, i, n, swaps)
+
+
+    # return swaps
+
+
+    for i in range(n // 2 -1, -1, -1):
+        j = 1
+
+        while True:
+            left_child = 2*j+1
+            right_child = 2*j+2
+            smallest = j
+            
+            if left_child < n and data[left_child] < data[smallest]:
+                smallest = left_child
+            if right_child < n and data[right_child] < data[smallest]:
+                smallest = right_child
+
+
+            if j != smallest:
+                swaps.append((j, smallest))
+                data[j], data[smallest] = data[smallest], data[j]
+                j = smallest
+
+            else:
+                break
 
     return swaps
 
+                
 
-def heapify(data, i, n, swaps):
-    left_child = 2 * i + 1
-    right_child = 2 * i + 2
-    smallest = i
+# def heapify(data, i, n, swaps):
+#     left_child = 2*i+1
+#     right_child = 2*i+2
+#     smallest = i
 
-    if left_child < n and data[left_child] < data[smallest]:
-        smallest = left_child
+#     if left_child < n and data[left_child] < data[smallest]:
+#         smallest = left_child
 
-    if right_child < n and data[right_child] < data[smallest]:
-        smallest = right_child
+#     if right_child < n and data[right_child] < data[smallest]:
+#         smallest = right_child
 
-    if smallest != i:
-        data[i], data[smallest] = data[smallest], data[i]
-        swaps.append((i, smallest))
-        heapify(data, smallest, n, swaps)
+#     if smallest != i:
+
+#         data[i], data[smallest] = data[smallest], data[i]
+
+#         swaps.append((i, smallest))
+
+#         heapify(data, smallest, n, swaps)
 
 
 def main():
-    input_method = input("Enter input method (I for keyboard input, F for file input): ")
 
-    if input_method == "I":
-        n = int(input("Enter number of elements: "))
-        data = list(map(int, input("Enter the elements: ").split()))
-    elif input_method == "F":
-        filename = input("Enter the filename: ")
-        with open(filename, "r") as file:
+    input_method = (input("Enter input method (I for keyboard input, F for file input): "))
+
+    if "I" in input_method:
+        n = int(input("enter number of elements: "))
+        data = list(map(int, input("enter the elements: ").split()))
+    if "F" in input_method:
+        filename = input("enter the filename:")
+        with open(f"tests/{filename}") as file:
             n = int(file.readline())
             data = list(map(int, file.readline().split()))
-    else:
-        print("Invalid input method")
-        return
 
+    # else:
+    #     print("Invalid input method")
+    #     return
+    
     assert len(data) == n
 
+   
     swaps = build_heap(data)
-
-    # output all swaps
+    
     print(len(swaps))
-    for i, j in swaps:
+    for i, j  in swaps:
         if i > j:
             i, j = j, i
+
         print(i, j)
+
 
 
 if __name__ == "__main__":
