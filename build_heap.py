7d6a76d25ba915f8c4bb523eb096dbb99795fe52
MegijaKristaMilune <125242855+MegijaKristaMilune@users.noreply.github.com>
2023-03-18 09:29:49 +0000

    test99

--- build_heap.py
+++ build_heap.py
@@ -4,40 +4,33 @@ def build_heap(data):
     swaps = []
     n = len(data)
   
-
-
     for i in range(n // 2 -1, -1, -1):
         heap(data, i, n, swaps)
     return swaps
 
-
-
-
-                
-
-def heap(data, i, n, swaps):
+ def heap(data, i, n, swaps):
     left_child = 2*i+1
     right_child = 2*i+2
-    smallest = i
+    last = i
 
     if left_child < n and data[left_child] < data[i]:
-        smallest = left_child
+        last = left_child
 
-    if right_child < n and data[right_child] < data[smallest]:
-        smallest = right_child
+    if right_child < n and data[right_child] < data[last]:
+        last = right_child
 
-    if smallest != i:
+    if last != i:
 
-        data[i], data[smallest] = data[smallest], data[i]
+        data[i], data[last] = data[last], data[i]
 
-        swaps.append((i, smallest))
+        swaps.append((i, last))
 
-        heap(data, smallest, n, swaps)
+        heap(data, last, n, swaps)
 
 
 def main():
 
-    input_method = (input("Enter input method (I for keyboard input, F for file input): "))
+    input_method = (input("enter input method I/F: "))
 
     if "I" in input_method:
         n = int(input("enter number of elements: "))
@@ -49,7 +42,6 @@ def main():
             data = list(map(int, file.readline().split()))
 
 
-    
     assert len(data) == n
 
    
@@ -57,10 +49,7 @@ def main():
     
     print(len(swaps))
     for i, j  in swaps:
-
-
         print(i, j)
-
 
 
 if __name__ == "__main__":
